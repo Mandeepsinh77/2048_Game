698a3f5b04e80c3e9a1b779c54b6a044ea1ce35b
Mandeepsinh Gohil <gohilmandipsinh242@gmail.com>
2024-05-09 21:26:46 +0530

    first commit

---
+++ game_display.py
@@ -0,0 +1,49 @@
+GRID_COLOR="#a39489"
+EMPTY_CELL_COLOR="#c2b3a9"
+SCORE_LABEL_FONT=("verdana",24)
+SCORE_FONT=("Helvetica",36,"bold")
+GAME_OVER_FONT_COLOR="#ffffff"
+WINNER_BG="#ffcc00"
+LOSER_BG="#a39489"
+
+CELL_COLORS={
+    2:"#fcefe6",
+    4:"#f2e8cb",
+    8:"#f5b682",
+    16:"#f29446",
+    32:"#ff775c",
+    64:"#e64c2e",
+    128:"#ede291",
+    256:"#fce130",
+    512:"#ffdb4a",
+    1024:"#f0b922",
+    2048:"#fad74d",
+}
+
+CELL_NUMBER_COLORS={
+    2:"#695c57",
+    4:"#695c57",
+    8:"#ffffff",
+    16:"#ffffff",
+    32:"#ffffff",
+    64:"#ffffff",
+    128:"#ffffff",
+    256:"#ffffff",
+    512:"#ffffff",
+    1024:"#ffffff",
+    2048:"#ffffff",
+}
+
+CELL_NUMBER_FONTS={
+     2:("Helverica",55,"bold"),
+    4:("Helverica",55,"bold"),
+    8:("Helverica",55,"bold"),
+    16:("Helverica",50,"bold"),
+    32:("Helverica",50,"bold"),
+    64:("Helverica",50,"bold"),
+    128:("Helverica",45,"bold"),
+    256:("Helverica",45,"bold"),
+    512:("Helverica",45,"bold"),
+    1024:("Helverica",40,"bold"),
+    2048:("Helverica",40,"bold"),
+}
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+m=Label(f1,text="mandeepsinh Gohil",bg="orange",fg="black",padx="20",pady="50")
+m.pack()
